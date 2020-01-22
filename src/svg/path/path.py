ded0160dd75514d6794fed0f30525abd63d09680
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2020-01-22 19:58:51 +0530

    Import ABC from collections.abc instead of collections for Python 3.9 compatibility.

--- src/svg/path/path.py
+++ src/svg/path/path.py
@@ -1,7 +1,11 @@
 from __future__ import division
 from math import sqrt, cos, sin, acos, degrees, radians, log, pi
-from collections import MutableSequence
 from bisect import bisect
+
+try:
+    from collections.abc import MutableSequence
+except ImportError:
+    from collections import MutableSequence
 
 # This file contains classes for the different types of SVG path segments as
 # well as a Path object that contains a sequence of path segments.
