f4be4e8da4b1d3fdee22b983c8ac77eb7864e175
Lennart <regebro@gmail.com>
2021-03-26 09:51:36 +0100

    New parser that treats flags correctly

--- src/svg/path/parser.py
+++ src/svg/path/parser.py
@@ -1,110 +1,189 @@
 # SVG Path specification parser
 
 import re
-from . import path
+from svg.path import path
 
 COMMANDS = set("MmZzLlHhVvCcSsQqTtAa")
 UPPERCASE = set("MZLHVCSQTA")
 
 COMMAND_RE = re.compile(r"([MmZzLlHhVvCcSsQqTtAa])")
-FLOAT_RE = re.compile(r"[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?")
+FLOAT_RE = re.compile(rb"^[-+]?[0-9]*\.?[0-9]+(?:[eE][-+]?[0-9]+)?")
+
+
+class InvalidPathError(ValueError):
+    pass
+
+
+# The argument sequences from the grammar, made sane.
+# u: Non-negative number
+# s: Signed number or coordinate
+# c: coordinate-pair, which is two coordinates/numbers, separated by whitespace
+# f: A one character flag, doesn't need whitespace, 1 or 0
+ARGUMENT_SEQUENCE = {
+    "M": "c",
+    "Z": "",
+    "L": "c",
+    "H": "s",
+    "V": "s",
+    "C": "ccc",
+    "S": "cc",
+    "Q": "cc",
+    "T": "c",
+    "A": "uusffc",
+}
+
+
+def strip_array(arg_array):
+    """Strips whitespace and commas"""
+    # EBNF wsp:(#x20 | #x9 | #xD | #xA) + comma: 0x2C
+    while arg_array and arg_array[0] in (0x20, 0x9, 0xD, 0xA, 0x2C):
+        arg_array[0:1] = b""
+
+
+def pop_number(arg_array):
+    res = FLOAT_RE.search(arg_array)
+    if not res:
+        raise InvalidPathError(f"Expected a number, got '{arg_array}'.")
+    number = float(res.group())
+    start = res.start()
+    end = res.end()
+    arg_array[start:end] = b""
+    strip_array(arg_array)
+
+    return number
+
+
+def pop_unsigned_number(arg_array):
+    number = pop_number(arg_array)
+    if number < 0:
+        raise InvalidPathError(f"Expected a non-negative number, got '{number}'.")
+    return number
+
+
+def pop_coordinate_pair(arg_array):
+    x = pop_number(arg_array)
+    y = pop_number(arg_array)
+    return complex(x, y)
+
+
+def pop_flag(arg_array):
+    flag = arg_array[0]
+    arg_array[0:1] = b""
+    strip_array(arg_array)
+    if flag == 48:  # ASCII 0
+        return False
+    if flag == 49:  # ASCII 1
+        return True
+
+
+FIELD_POPPERS = {
+    "u": pop_unsigned_number,
+    "s": pop_number,
+    "c": pop_coordinate_pair,
+    "f": pop_flag,
+}
+
+
+def _commandify_path(pathdef):
+    """Splits path into commands and arguments"""
+    token = None
+    for x in COMMAND_RE.split(pathdef):
+        x = x.strip()
+        if x in COMMANDS:
+            if token is not None:
+                yield token
+            if x in ("z", "Z"):
+                # The end command takes no arguments, so add a blank one
+                token = (x, "")
+            else:
+                token = (x,)
+        elif x:
+            if token is None:
+                raise InvalidPathError(f"Path does not start with a command: {pathdef}")
+            token += (x,)
+    yield token
 
 
 def _tokenize_path(pathdef):
-    for x in COMMAND_RE.split(pathdef):
-        if x in COMMANDS:
-            yield x
-        for token in FLOAT_RE.findall(x):
-            yield token
+    for command, args in _commandify_path(pathdef):
+        # Shortcut this for the close command, that doesn't have arguments:
+        if command in ("z", "Z"):
+            yield (command,)
+            continue
+
+        # For the rest of the commands, we parse the arguments and
+        # yield one command per full set of arguments
+        arg_sequence = ARGUMENT_SEQUENCE[command.upper()]
+        arguments = bytearray(args, "ascii")
+        while arguments:
+            command_arguments = []
+            for arg in arg_sequence:
+                try:
+                    command_arguments.append(FIELD_POPPERS[arg](arguments))
+                except InvalidPathError as e:
+                    raise InvalidPathError(
+                        f"Invalid path element {command} {args}"
+                    ) from e
+
+            yield (command,) + tuple(command_arguments)
+
+            # Implicit Moveto commands should be treated as Lineto commands.
+            if command == "m":
+                command = "l"
+            elif command == "M":
+                command = "L"
 
 
-def parse_path(pathdef, current_pos=0j):
-    # In the SVG specs, initial movetos are absolute, even if
-    # specified as 'm'. This is the default behavior here as well.
-    # But if you pass in a current_pos variable, the initial moveto
-    # will be relative to that current_pos. This is useful.
-    elements = list(_tokenize_path(pathdef))
-    # Reverse for easy use of .pop()
-    elements.reverse()
-
+def parse_path(pathdef):
     segments = path.Path()
     start_pos = None
-    command = None
+    last_command = None
+    current_pos = 0
 
-    while elements:
-
-        if elements[-1] in COMMANDS:
-            # New command.
-            last_command = command  # Used by S and T
-            command = elements.pop()
-            absolute = command in UPPERCASE
-            command = command.upper()
-        else:
-            # If this element starts with numbers, it is an implicit command
-            # and we don't change the command. Check that it's allowed:
-            if command is None:
-                raise ValueError(
-                    "Unallowed implicit command in %s, position %s"
-                    % (pathdef, len(pathdef.split()) - len(elements))
-                )
-            last_command = command  # Used by S and T
-
+    for token in _tokenize_path(pathdef):
+        command = token[0]
+        absolute = command.isupper()
+        command = command.upper()
         if command == "M":
-            # Moveto command.
-            x = elements.pop()
-            y = elements.pop()
-            pos = float(x) + float(y) * 1j
+            pos = token[1]
             if absolute:
                 current_pos = pos
             else:
                 current_pos += pos
             segments.append(path.Move(current_pos))
-            # when M is called, reset start_pos
-            # This behavior of Z is defined in svg spec:
-            # http://www.w3.org/TR/SVG/paths.html#PathDataClosePathCommand
             start_pos = current_pos
 
-            # Implicit moveto commands are treated as lineto commands.
-            # So we set command to lineto here, in case there are
-            # further implicit commands after this moveto.
-            command = "L"
-
         elif command == "Z":
-            # Close path
             segments.append(path.Close(current_pos, start_pos))
             current_pos = start_pos
-            start_pos = None
-            command = None  # You can't have implicit commands after closing.
 
         elif command == "L":
-            x = elements.pop()
-            y = elements.pop()
-            pos = float(x) + float(y) * 1j
+            pos = token[1]
             if not absolute:
                 pos += current_pos
             segments.append(path.Line(current_pos, pos))
             current_pos = pos
 
         elif command == "H":
-            x = elements.pop()
-            pos = float(x) + current_pos.imag * 1j
+            hpos = token[1]
             if not absolute:
-                pos += current_pos.real
+                hpos += current_pos.real
+            pos = complex(hpos, current_pos.imag)
             segments.append(path.Line(current_pos, pos))
             current_pos = pos
 
         elif command == "V":
-            y = elements.pop()
-            pos = current_pos.real + float(y) * 1j
+            vpos = token[1]
             if not absolute:
-                pos += current_pos.imag * 1j
+                vpos += current_pos.imag
+            pos = complex(current_pos.real, vpos)
             segments.append(path.Line(current_pos, pos))
             current_pos = pos
 
         elif command == "C":
-            control1 = float(elements.pop()) + float(elements.pop()) * 1j
-            control2 = float(elements.pop()) + float(elements.pop()) * 1j
-            end = float(elements.pop()) + float(elements.pop()) * 1j
+            control1 = token[1]
+            control2 = token[2]
+            end = token[3]
 
             if not absolute:
                 control1 += current_pos
@@ -117,31 +196,30 @@ def parse_path(pathdef, current_pos=0j):
         elif command == "S":
             # Smooth curve. First control point is the "reflection" of
             # the second control point in the previous path.
-
-            if last_command not in "CS":
-                # If there is no previous command or if the previous command
-                # was not an C, c, S or s, assume the first control point is
-                # coincident with the current point.
-                control1 = current_pos
-            else:
-                # The first control point is assumed to be the reflection of
-                # the second control point on the previous command relative
-                # to the current point.
-                control1 = current_pos + current_pos - segments[-1].control2
-
-            control2 = float(elements.pop()) + float(elements.pop()) * 1j
-            end = float(elements.pop()) + float(elements.pop()) * 1j
+            control2 = token[1]
+            end = token[2]
 
             if not absolute:
                 control2 += current_pos
                 end += current_pos
 
+            if last_command in "CS":
+                # The first control point is assumed to be the reflection of
+                # the second control point on the previous command relative
+                # to the current point.
+                control1 = current_pos + current_pos - segments[-1].control2
+            else:
+                # If there is no previous command or if the previous command
+                # was not an C, c, S or s, assume the first control point is
+                # coincident with the current point.
+                control1 = current_pos
+
             segments.append(path.CubicBezier(current_pos, control1, control2, end))
             current_pos = end
 
         elif command == "Q":
-            control = float(elements.pop()) + float(elements.pop()) * 1j
-            end = float(elements.pop()) + float(elements.pop()) * 1j
+            control = token[1]
+            end = token[2]
 
             if not absolute:
                 control += current_pos
@@ -153,37 +231,41 @@ def parse_path(pathdef, current_pos=0j):
         elif command == "T":
             # Smooth curve. Control point is the "reflection" of
             # the second control point in the previous path.
+            end = token[1]
 
-            if last_command not in "QT":
-                # If there is no previous command or if the previous command
-                # was not an Q, q, T or t, assume the first control point is
-                # coincident with the current point.
-                control = current_pos
-            else:
+            if not absolute:
+                end += current_pos
+
+            if last_command in "QT":
                 # The control point is assumed to be the reflection of
                 # the control point on the previous command relative
                 # to the current point.
                 control = current_pos + current_pos - segments[-1].control
-
-            end = float(elements.pop()) + float(elements.pop()) * 1j
-
-            if not absolute:
-                end += current_pos
+            else:
+                # If there is no previous command or if the previous command
+                # was not an Q, q, T or t, assume the first control point is
+                # coincident with the current point.
+                control = current_pos
 
             segments.append(path.QuadraticBezier(current_pos, control, end))
             current_pos = end
 
         elif command == "A":
-            radius = float(elements.pop()) + float(elements.pop()) * 1j
-            rotation = float(elements.pop())
-            arc = float(elements.pop())
-            sweep = float(elements.pop())
-            end = float(elements.pop()) + float(elements.pop()) * 1j
+            # For some reason I implemented the Arc with a complex radius.
+            # That doesn't really make much sense, but... *shrugs*
+            radius = complex(token[1], token[2])
+            rotation = token[3]
+            arc = token[4]
+            sweep = token[5]
+            end = token[6]
 
             if not absolute:
                 end += current_pos
 
             segments.append(path.Arc(current_pos, radius, rotation, arc, sweep, end))
             current_pos = end
+
+        # Finish up the loop in preparation for next command
+        last_command = command
 
     return segments
